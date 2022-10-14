e11367eec6ec3c3e81031c178142730db3156b0f
dielleaiko@hotmail.com <dielleaiko@hotmail.com>
2022-10-13 22:38:43 -0400

    Made changes to file

--- PDF To Audio/PDF2Audio.py
+++ PDF To Audio/PDF2Audio.py
@@ -9,11 +9,11 @@ window.geometry('500x350')
 window.iconbitmap('https://github.com/Gear-I/Small-Projects/blob/main/PDF%20To%20Audio/icons8-audiobook-64.ico')
 window.config(bg="orange")
 
-startingpagenumber = Entry(Window)
-page1.place(relx=0.02,rely=0.1)
+startingpagenumber = Entry(window)
 startingpagenumber.place(relx=0.6,rely=0.1)
-page1=Label(Window,text=”Enter starting page number”)
-label = Label(Window, text="select a book.")
+page1=Label(window,text=”Enter starting page number”)
+page1.place(relx=0.02,rely=0.1)
+label = Label(window, text=("select a book.")
 label.place(relx=0.3,rely=0.2)
 
 
@@ -30,7 +30,7 @@ def file():
         speaker.say(txt)
         speaker.runAndWait()
  
-B=Button(Window, text="Choose  the Book", command=file)
+B=Button(window, text=("Choose  the Book"), command=file)
 B.place(relx=0.4,rely=0.3)
  
 mainloop()
