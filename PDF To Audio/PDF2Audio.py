e093f1fcd4f3ce01276fd624bf3596ae045172cc
dielleaiko@hotmail.com <dielleaiko@hotmail.com>
2022-10-13 22:12:07 -0400

    Imported Modules

--- PDF To Audio/PDF2Audio.py
+++ PDF To Audio/PDF2Audio.py
@@ -1,1 +1,9 @@
+from tkinter import *
+from tkinter import filedialog
+import pyttsx3
+import PyPDF2
 
+window = Tk()
+window.title('PDF To Audio Converter Version 1.0')
+window.geometry('500x350')
+window.config(bg="orange")
