f4e1c70d41d6a8ad0b8fe638622cab41b89c4ccd
dielleaiko@hotmail.com <dielleaiko@hotmail.com>
2022-10-13 22:44:32 -0400

    Changes to file

--- PDF To Audio/PDF2Audio.py
+++ PDF To Audio/PDF2Audio.py
@@ -2,18 +2,18 @@ from tkinter import *
 from tkinter import filedialog
 import pyttsx3
 import PyPDF2
+import urllib
 
 window = Tk()
 window.title('PDF To Audio Converter Version 1.0')
 window.geometry('500x350')
-window.iconbitmap('https://github.com/Gear-I/Small-Projects/blob/main/PDF%20To%20Audio/icons8-audiobook-64.ico')
 window.config(bg="orange")
 
 startingpagenumber = Entry(window)
 startingpagenumber.place(relx=0.6,rely=0.1)
-page1=Label(window,text=”Enter starting page number”)
+page1=Label(window,text=('Enter starting page number'))
 page1.place(relx=0.02,rely=0.1)
-label = Label(window, text=("select a book.")
+label = Label(window, text="select a book.")
 label.place(relx=0.3,rely=0.2)
 
 
@@ -30,7 +30,7 @@ def file():
         speaker.say(txt)
         speaker.runAndWait()
  
-B=Button(window, text=("Choose  the Book"), command=file)
+B=Button(window, text="Choose  the Book", command=file)
 B.place(relx=0.4,rely=0.3)
  
 mainloop()
