c7cd7670fff6831407aee5461d407fabb32cf0bf
dielleaiko@hotmail.com <dielleaiko@hotmail.com>
2022-10-22 15:10:41 -0400

    Made changes to the code

--- Calculator.c
+++ Calculator.c
@@ -1,44 +1,44 @@
 // Welcome to a simple calculator
+
+
 #include <stdio.h>
 #include <stdlib.h>
 
 int main()
-
 {
-	char ch;
-	double a, b;
-	while (1) {
-		printf("Welcome to a simple calculator");
-		printf("Enter an opertion either, (+, -, *, /), if want to exit press x: ")
-		scanf("%c", &ch);
-		//To Exit
-		if (ch == 'x')
-			exit(0);
-		printf("Enter first and second number: ");
-		scanf("%lf %lf", &a, &b);
-		//Different Operations Options
-		//Addition
-		if (ch == '+') {
-			printf(" % .1lf + % .1lf = % .1lf\n", a, b, a + b); 
-		}
-		//Subtraction
-		else if (ch == '-') {
-			printf("%.1lf - %.1lf = %.1lf\n", a, b, a - b);
-		}
-		//Multiplcation
-		else if (ch == '*') {
-			printf("%.1lf * %.1lf = %.1lf\n", a, b, a * b);
-		}
-		//Division 
-		else if (ch == '/') {
-			printf("%.1lf / %.1lf = %.1lf\n", a, b, a / b);
-		}
-		else {
-			// If operator doesn't match any case constant
-			printf("Error! Enter a valid operator\n");
-
-		}
-
-		}
+    char ch;
+    double a, b;
+    while (1) {
+        printf("Welcome to Calculator")
+        printf("Enter an operation of your choice (+, -, *, /), if want to exit press x: ");
+        scanf(" %c", &ch);
+        // to exit
+        if (ch == 'x')
+            exit(0);
+        printf("Enter two numbers: ");
+        scanf("%lf %lf", &a, &b);
+        //Different Operations
+        if (ch == '+') {
+            // For Addition
+            printf("%.1lf + %.1lf = %.1lf\n", a, b, a + b);
+        }
+        else if (ch == '-') {
+            // For Subtraction
+            printf("%.1lf - %.1lf = %.1lf\n", a, b, a - b);
+        }
+        else if (ch == '*') {
+            // For Multiplication
+            printf("%.1lf * %.1lf = %.1lf\n", a, b, a * b);
+        }
+        else if (ch == '/') {
+            // For Division
+            printf("%.1lf / %.1lf = %.1lf\n", a, b, a / b);
+        }
+        else {
+            // If operation does not match
+            printf("Error! please write a valid operator\n");
+        }
+    }
+}
 
 		}
