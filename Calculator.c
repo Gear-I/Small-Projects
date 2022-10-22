8f42469c6cfea066aeb50298fa037ab2ad767a02
dielleaiko@hotmail.com <dielleaiko@hotmail.com>
2022-10-22 15:58:34 -0400

    Back to orginal

--- Calculator.c
+++ Calculator.c
@@ -3,43 +3,39 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <conio.h>
 
 int main()
 {
     char ch;
     double a, b;
     while (1) {
-        textcolor(RED);
-        cprintf(" Welcome to the calculator\nEnter an operation of your choice (+, -, *, /), if want to exit press x: ");
+        printf(" Welcome to the calculator\nEnter an operation of your choice (+, -, *, /), if want to exit press x: ");
         scanf(" %c", &ch);
         // to exit
         if (ch == 'x')
             exit(0);
-        cprintf("Enter two numbers: ");
+        printf("Enter two numbers: ");
         scanf("%lf %lf", &a, &b);
         //Different Operations
         if (ch == '+') {
             // For Addition
-            cprintf("%.1lf + %.1lf = %.1lf\n", a, b, a + b);
+            printf("%.1lf + %.1lf = %.1lf\n", a, b, a + b);
         }
         else if (ch == '-') {
             // For Subtraction
-            cprintf("%.1lf - %.1lf = %.1lf\n", a, b, a - b);
+            printf("%.1lf - %.1lf = %.1lf\n", a, b, a - b);
         }
         else if (ch == '*') {
             // For Multiplication
-            cprintf("%.1lf * %.1lf = %.1lf\n", a, b, a * b);
+            printf("%.1lf * %.1lf = %.1lf\n", a, b, a * b);
         }
         else if (ch == '/') {
             // For Division
-            cprintf("%.1lf / %.1lf = %.1lf\n", a, b, a / b);
+            printf("%.1lf / %.1lf = %.1lf\n", a, b, a / b);
         }
         else {
             // If operation does not match
-            cprintf("Error! please enter a valid operation\n");
-            getch()
-                return 0;
+            printf("Error! please enter a valid operation\n");
         }
     }
 }
