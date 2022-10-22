783c0dcbe03a2f56c033908d80043c3c71b01134
dielleaiko@hotmail.com <dielleaiko@hotmail.com>
2022-10-22 16:26:58 -0400

    Added colors to text and background

--- Calculator.c
+++ Calculator.c
@@ -3,9 +3,14 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <windows.h>
+
 
 int main()
 {
+    //E = Color yellow
+    //2 = Green Background color
+    system("Color 2E");
     char ch;
     double a, b;
     while (1) {
