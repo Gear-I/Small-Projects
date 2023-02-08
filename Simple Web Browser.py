eea3d02873996dd9e0ee7c52aa47de51251d28ec
dielleaiko@hotmail.com <dielleaiko@hotmail.com>
2023-02-07 21:01:30 -0500

    Starting Browser Window.

---
+++ Simple Web Browser.py
@@ -0,0 +1,19 @@
+# importing neccessary libraries
+
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtWebEngineWidgets import *
+from PyQt5.QtPrintSupport import *
+import os
+import sys
+
+# Creating Main Window for Browser
+
+class MainWindow(QMainWindow):
+
+	# Constructor
+	def _init_(self, *args, **kwargs):
+		super(MainWindow, self)._init_(*args, **kwargs)
+
+
