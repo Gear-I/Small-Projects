0ac3552cc48b4f5f6c0ac8c55fdf1f9c2b037b30
Dielle De Noon <99276218+Gear-I@users.noreply.github.com>
2023-03-09 14:19:08 -0500

    Adding progress function

---
+++ Port_Scanner_v2.ps1
@@ -0,0 +1,23 @@
+function PortScanner {
+param($ip = "localhost")
+$Socket = New-Object System.Net.Socket.TcpClient
+    $Ports = [ordered]@ {
+                    FTP=21
+                    SSH=22
+                    Telnet=23
+                    Http=80
+                    Https=443
+                    RDP=3389
+                     }
+    foreach ($port in $Ports.Values) {
+        try {
+            Write-Host "Connecting to ip $IP with Port #$port"
+             $Socket.Connect($ip , $port)
+            for ($i = 1, $i -le 100; $i++)
+            {
+                Write-Progress -Activity "Searching for ports status" -status "$i% Complete:" -PercentComplete $i
+                Start-sleep -Millliseconds 250
+            }
+        }
+    }
+}
